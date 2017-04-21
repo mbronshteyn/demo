5d804608d1bc982ceb4f736a2b4c1dbd4201a0de
Bronshteyn <mbronshteyn@sdgl07d392dcf.corp.intuit.net>
2017-04-21 13:06:30 -0700

    add message xsd and jaxb maven plugin

--- src/main/java/com/mbronshteyn/service/AuthService.java
+++ src/main/java/com/mbronshteyn/service/AuthService.java
@@ -1,8 +1,14 @@
 package com.mbronshteyn.service;
 
+import com.mbronshteyn.message.User;
+
 /**
  * Created by mbronshteyn on 4/15/17.
  */
 public class AuthService {
+
+    public void addUser( User user ){
+        //
+    }
 
 }
