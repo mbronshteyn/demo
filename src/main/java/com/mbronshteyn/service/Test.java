5df84e3d00738c5b8cf16a2bad814fa683d127e8
Bronshteyn <mbronshteyn@SDGL147a0e261.local>
2017-05-08 15:29:32 -0700

    add ws classes

--- src/main/java/com/mbronshteyn/service/Test.java
+++ src/main/java/com/mbronshteyn/service/Test.java
@@ -1,7 +1,23 @@
 package com.mbronshteyn.service;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * Created by mbronshteyn on 4/25/17.
  */
 public class Test {
+    public static void main( String[] args ){
+        test();
+        return;
+    }
+
+    private static void test() {
+        String[] a =  { "a", "b", "c" };
+        List<String> list = Arrays.asList( a );
+        int count = list.size();
+        List<Integer> integers = Arrays.asList(count);
+        System.out.println( integers.toString());
+    }
 }
--- src/main/java/com/mbronshteyn/ws/UserWS.java
+++ src/main/java/com/mbronshteyn/ws/UserWS.java
@@ -1,7 +1,18 @@
 package com.mbronshteyn.ws;
 
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import com.mbronshteyn.ws.User;
+
+
 /**
  * Created by mbronshteyn on 4/24/17.
  */
+@Path("/user")
 public class UserWS {
+
+    @POST
+    public void addUser( User user ){
+
+    }
 }
