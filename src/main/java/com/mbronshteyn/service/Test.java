4daa2b1873274b148cf756b520301f7548c78aac
Bronshteyn <mbronshteyn@SDGL147a0e261.local>
2017-05-08 15:28:47 -0700

    add ws classes

---
+++ src/main/java/com/mbronshteyn/service/Test.java
@@ -0,0 +1,7 @@
+package com.mbronshteyn.service;
+
+/**
+ * Created by mbronshteyn on 4/25/17.
+ */
+public class Test {
+}
---
+++ src/main/java/com/mbronshteyn/ws/UserWS.java
@@ -0,0 +1,7 @@
+package com.mbronshteyn.ws;
+
+/**
+ * Created by mbronshteyn on 4/24/17.
+ */
+public class UserWS {
+}
