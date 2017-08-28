fae193d40a26f2236656dd4c12218d4fdefc8c31
mbronshteyn <michael.bronshteyn@gmail.com>
2017-08-28 13:56:55 -0700

    add comment to specify jvm option to generate heap dump

--- src/main/java/com/mbronshteyn/heapdump/TestHeapDump.java
+++ src/main/java/com/mbronshteyn/heapdump/TestHeapDump.java
@@ -1,5 +1,10 @@
 package com.mbronshteyn.heapdump;
 
+/**
+ * Heap Dump example.
+ * Don't forget to add jvm parameters:
+ * -XX:+HeapDumpOnOutOfMemoryError -Xms10m -Xmx1g
+ */
 public class TestHeapDump {
     public static void main(String[] args) {
        A a =  new A();
